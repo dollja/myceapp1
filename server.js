252f91d615700a0ef8c55f0cf03bebc3ba752c54
Jacky-Doll <jacky.doll@gmail.com>
2023-07-14 18:32:37 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const http = require('http');
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end( "Hello world\n" );
+  response.end( "Hello jacky\n" );
 }).listen(8080);
